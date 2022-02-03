ba834f36e3f2987c64c3cf5ef5fc334c2dbd6206
İren SALTALI <iren@saltali.com>
2022-02-03 22:38:11 +0300

    WIP: to saved position

--- src/skarsta.cpp
+++ src/skarsta.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-#include <util/atomic.h> // For the ATOMIC_BLOCK macro
 #include <EEPROM.h>
 #include <FreeButton.h>
 
@@ -19,8 +18,6 @@
 #define SAVE3 12
 #define RESET 13
 
-void callBack();
-
 // Buttons.
 FreeButton upButton(UP);
 FreeButton downButton(DOWN);
@@ -33,24 +30,20 @@ FreeButton resetButton(RESET);
 #define RIS A3
 #define LIS A1
 
-const int LONG_PRESS_TIME = 2000; // 2 seconds
+const int LONG_PRESS_TIME = 1000; // 1 seconds
 
 int out1;
 int out2;
-long pre=-1;
-int direct=0;
-volatile long posi = 0;
-bool newOpen=false;
+volatile long position = 0;
 
 void readEncoder();
-void savePos1();
+void savePosition1();
+void savePosition2();
+void savePosition3();
 void goUp();
 void goDown();
+void goTo1();
 void stopMotor();
-String buttonRead();
-void onPressedForDuration();
-void onPressed();
-int digiRead(int buttonPin);
 
 void setup() {
   Serial.begin(9600);
@@ -64,91 +57,90 @@ void setup() {
   digitalWrite(LEN,HIGH);
   attachInterrupt(digitalPinToInterrupt(ENCA),readEncoder,RISING);
   
-
-  save3Button.Read();
-  // save3Button.OnPressedForDuration(onPressedForDuration, LONG_PRESS_TIME);
-  resetButton.OnPressed(callBack);
   upButton.OnPressed(goUp);
   upButton.OnUnPressed(stopMotor);
-
-  Serial.println("EEPROM.length():"+String(EEPROM.length()));
-  newOpen=true;
+  downButton.OnPressed(goDown);
+  downButton.OnUnPressed(stopMotor);
+  save1Button.OnPressed(goTo1);
+  save1Button.OnPressedForDuration(savePosition1,LONG_PRESS_TIME);
+  // save2Button.OnPressedForDuration(savePosition2,LONG_PRESS_TIME);
+  // save3Button.OnPressedForDuration(savePosition3,LONG_PRESS_TIME);
 }
 
 void callBack(){
   Serial.println("Called back");
 }
 
+void callBackForDuration(){
+  Serial.println("callBackForDuration");
+}
+
 void loop() {
-  buttonRead();
   upButton.Read();
   downButton.Read();
   save1Button.Read();
   save2Button.Read();
   save3Button.Read();
   resetButton.Read();
-    
-  
-  if(digitalRead(SAVE1)==HIGH && false){
-    Serial.println("SAVE1");
-    savePos1();
-  }
-    
-  long ris=  (analogRead(RIS)/ 1023.0)*5000;
-  long lis = (analogRead(LIS)/1023.0)*5000;
+}
 
-  // Serial.println("ris:");
-  // Serial.println(ris);
-  // Serial.println("lis:");
-  // Serial.println(lis);
+void writeLongIntoEEPROM(int address, long number)
+{ 
+  EEPROM.write(address, (number >> 24) & 0xFF);
+  EEPROM.write(address + 1, (number >> 16) & 0xFF);
+  EEPROM.write(address + 2, (number >> 8) & 0xFF);
+  EEPROM.write(address + 3, number & 0xFF);
+}
 
-  
-  
-  long pos = 0; 
-  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-    pos = posi;
-  }
-
-
-
-  byte gotopos=0;
-  EEPROM.get(0,gotopos);
-  
-  if(gotopos>0 && newOpen && false){
-    Serial.println("gotopos:"+String(gotopos));
-    goDown();
-    gotopos--;
-    EEPROM.write(0,gotopos);
-    delay(300);
-    if(gotopos==0)
-      newOpen=false;
-  }
-  
+long readLongFromEEPROM(int address){
+  return ((long)EEPROM.read(address) << 24) +
+         ((long)EEPROM.read(address + 1) << 16) +
+         ((long)EEPROM.read(address + 2) << 8) +
+         (long)EEPROM.read(address + 3);
 }
 
 void readEncoder(){
   int b = digitalRead(ENCB);
   if(b > 0){
-    posi++;
+    position--;
   }
   else{
-    posi--;
+    position++;
   }
 }
 
-void savePos1(){
-  byte a=3;
-  EEPROM.write(0,a);
+void savePosition1(){
+  Serial.println("position: "+ String(position));
+  writeLongIntoEEPROM(0, position);
+}
+// void savePosition2(){
+//   EEPROM.write(1,position);
+// }
+// void savePosition3(){
+//   EEPROM.write(2,position);
+// }
+
+void goTo1(){
+  long target = readLongFromEEPROM(0);
+  while(target!=position){
+    Serial.println("position: "+ String(position));
+    if(position<target){
+      goUp();
+    }
+    else if(position>target){
+      goDown();
+    }
+  }
 }
 
 void goUp(){
-  Serial.println("goUp");
+  // Serial.println("goUp");
   analogWrite(LPWM,0);
   analogWrite(RPWM,255);
 }
 
 void goDown(){
-  Serial.println("goDown");
+  // Serial.println("goDown");
   analogWrite(LPWM,255);
   analogWrite(RPWM,0);
 }
@@ -157,43 +149,4 @@ void stopMotor(){
   Serial.println("stopMotor");
   analogWrite(RPWM,0);
   analogWrite(LPWM,0);
-}
-
-
-// Button Reads
-int UP_LAST_STATE=LOW;
-unsigned long UP_PRESSED_TIME =0; 
-int DOWN_LAST_STATE=LOW;
-unsigned long DOWN_PRESSED_TIME =0;
-int SAVE1_LAST_STATE=LOW;
-unsigned long SAVE1_PRESSED_TIME =0;
-int SAVE2_LAST_STATE=LOW;
-unsigned long SAVE2_PRESSED_TIME =0;
-int SAVE3_LAST_STATE=LOW;
-unsigned long SAVE3_PRESSED_TIME =0;
-int RESET_LAST_STATE=LOW;
-unsigned long RESET_PRESSED_TIME =0;
-
-String buttonRead(){
-  int SAVE3_CURRENT_STATE = digiRead(SAVE3);
-  if(SAVE3_LAST_STATE==LOW && SAVE3_CURRENT_STATE==HIGH){
-    SAVE3_PRESSED_TIME=millis();
-    SAVE3_LAST_STATE=SAVE3_CURRENT_STATE;    
-  }
-  else if(SAVE3_LAST_STATE==HIGH && SAVE3_CURRENT_STATE==LOW){
-    unsigned long dif= millis()-SAVE3_PRESSED_TIME;
-    if(dif>LONG_PRESS_TIME)
-      Serial.println("Long press SAVE3");
-    Serial.println("dif:"+String(dif));
-    SAVE3_LAST_STATE=SAVE3_CURRENT_STATE;
-    SAVE3_PRESSED_TIME=0;
-  }
-  return " ";
-}
-
-
-int digiRead(int buttonPin) {
-  if(digitalRead(buttonPin)==HIGH)
-    return digitalRead(buttonPin);
-  return LOW;
 }
